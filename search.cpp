8efb34e1c9fbd39cc0d6c6825558f6192c09d705
prime <prime@kmc.gr.jp>
2017-08-23 13:38:43 +0900

    implement alpha beta method

--- search.cpp
+++ search.cpp
@@ -3,19 +3,21 @@
 #include <utility>
 #include "eval.hpp"
 
-int search_eval(const Board &bd, const Turn &turn, int depth) {
+int search_eval(const Board &bd, const Turn &turn, int depth, int alpha, int beta) {
   if (depth == 0) return eval(bd, turn);
   int max_score = -INF;
   for (int x = 1; x <= 8; ++x) {
     for (int y = 1; y <= 8; ++y) {
       if (movable(bd, turn, Position(x, y))) {
         Board next = move(bd, turn, Position(x, y));
-        max_score = std::max(max_score, -search_eval(next, opponent(turn), depth-1));
+        max_score = std::max(max_score, -search_eval(next, opponent(turn), depth-1, -beta, -alpha));
+        if (max_score >= beta) return max_score;
+        alpha = std::max(alpha, max_score);
       }
     }
   }
   if (max_score == -INF) {
-    return -search_eval(bd, opponent(turn), depth-1);
+    return -search_eval(bd, opponent(turn), depth-1, -beta, -alpha);
   }
   return max_score;
 }
@@ -27,7 +29,7 @@ Position search(const Board &bd, const Turn &turn) {
     for (int y = 1; y <= 8; ++y) {
       if (movable(bd, turn, Position(x, y))) {
         Board next = move(bd, turn, Position(x, y));
-        int score = -search_eval(next, opponent(turn), 3);
+        int score = -search_eval(next, opponent(turn), 6, -INF, -max_score);
         if (score > max_score) {
           max_score = score;
           max_score_pos = Position(x, y);
